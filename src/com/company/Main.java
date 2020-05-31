9e5acbb8d32d626f37eaf5f503860224e6bace1c
nastaran azani <azaninastaran@gmail.com>
2020-05-31 07:43:20 -0400

    initial git

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//write your code here
+        double side1=0;
+        double side2=0;
+        double hype=0;
+        side1=8;
+        side2=6;
+        hype=Math.sqrt(Math.pow(side1,2)+Math.pow(side2,2));
+        System.out.println(hype);
+    }
+}
